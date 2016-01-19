c11700a929ad0a56d1c2e660c962ad5572aa0c0c
Andrew Duthie <aduthie7@gmail.com>
2016-01-18 21:43:05 -0500

    Use express for proper routing to base HTML

--- server.js
+++ server.js
@@ -1,6 +1,44 @@
-var server = new ( require( 'node-static' ) ).Server( './www' );
-require( 'http' ).createServer( function( request, response ) {
-	request.addListener( 'end', function() {
-		server.serve( request, response );
-	} ).resume();
-} ).listen( process.env.PORT || 3000 );
+/**
+ * External dependencies
+ */
+
+var express = require( 'express' ),
+	compression = require( 'compression' ),
+	http = require( 'http' );
+
+/**
+ * App initialization
+ */
+
+var app = express(),
+	server = http.Server( app );
+
+/**
+ * Module variables
+ */
+
+var port = process.env.PORT || 3000;
+
+/**
+ * Middlewares
+ */
+
+app.use( compression() );
+app.use( express.static( __dirname + '/www' ) );
+
+/**
+ * Routes
+ */
+
+app.get( '*', function( request, response ) {
+	response.sendFile( __dirname + '/www/index.html' );
+} );
+
+/**
+ * Start server
+ */
+
+server.listen( port, function() {
+	/* eslint-disable no-console */
+	console.log( 'Listening on port %d...', port );
+} );
