9e0b2e25fc6ae3367fcca739176bb018d196ff07
Andrew Duthie <aduthie7@gmail.com>
2016-02-07 20:17:53 -0500

    Add meta description

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -28,6 +28,7 @@ module.exports = {
 				'<html manifest="/appcache/manifest.appcache">' +
 				'<head>' +
 				'	<meta name="viewport" content="width=device-width">' +
+				'	<meta name="description" content="Regimen helps with your workout progression planning. Supporting many popular routines, achieve your goals with a simple-to-use interface.">' +
 				'	<meta name="theme-color" content="#63bdd4">' +
 				'	<meta name="apple-mobile-web-app-capable" content="yes">' +
 				'	<title>Regimen</title>' +
