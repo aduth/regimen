56b72f75f97f98e45bb98cacecceefa6402b0ec1
Andrew Duthie <aduthie7@gmail.com>
2016-02-07 22:36:00 -0500

    Allow children to be passed as icon screen reader text

--- src/components/icon/index.jsx
+++ src/components/icon/index.jsx
@@ -5,14 +5,23 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
-function Icon( { icon } ) {
+function Icon( { icon, children } ) {
 	const classes = classNames( 'icon', 'fa', 'fa-' + icon );
 
-	return <span className={ classes } />;
+	return (
+		<span className={ classes }>
+			{ children && (
+				<span className="icon__screen-reader-text">
+					{ children }
+				</span>
+			) }
+		</span>
+	);
 }
 
 Icon.propTypes = {
-	icon: PropTypes.string.isRequired
+	icon: PropTypes.string.isRequired,
+	children: PropTypes.node
 };
 
 export default Icon;
