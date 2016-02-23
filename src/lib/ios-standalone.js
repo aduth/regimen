b564a43e44fbe3ebd5861e912a8461c21d36a65e
Andrew Duthie <andrew@andrewduthie.com>
2016-02-22 19:33:33 -0500

    Force page reload on iOS standalone appcache update

--- src/lib/ios-standalone.js
+++ src/lib/ios-standalone.js
@@ -4,14 +4,21 @@
 
 import { pushPath } from 'redux-simple-router';
 
-export default function( store ) {
-	if ( ! navigator.standalone || ! /(iPad|iPhone|iPod)/.test( navigator.userAgent ) ) {
-		return;
-	}
-
-	// Apply page styling
+/**
+ * Adds iOS-specific styling to the page.
+ */
+function applyStyling() {
 	document.documentElement.classList.add( 'is-ios-standalone' );
+}
 
+/**
+ * Tracks and restores the current window location path. This is necessary
+ * because otherwise the standalone app will always resume at the path at
+ * which the user had saved the page to their home screen.
+ *
+ * @param {Object} store Redux store instance
+ */
+function trackPath( store ) {
 	// Restore path from localStorage
 	let path = localStorage.getItem( 'path' );
 	if ( path && path !== store.getState().routing.path ) {
@@ -26,4 +33,36 @@ export default function( store ) {
 			localStorage.setItem( 'path', path );
 		}
 	} );
+}
+
+/**
+ * Binds to the window applicationCache, forcing a reload without cache when
+ * it's determined that the application cache has an update ready. This is
+ * necessary because otherwise the changes aren't reflected until the next
+ * time the application is loaded.
+ */
+function reloadOnCacheUpdate() {
+	if ( ! window.applicationCache ) {
+		return;
+	}
+
+	window.applicationCache.addEventListener( 'updateready', () => {
+		window.location.reload( true );
+	} );
+}
+
+/**
+ * Enables compatibility features for iOS standalone (home screen) usage,
+ * because iOS is misery.
+ *
+ * @param {Object} store Redux store instance
+ */
+export default function( store ) {
+	if ( ! navigator.standalone || ! /(iPad|iPhone|iPod)/.test( navigator.userAgent ) ) {
+		return;
+	}
+
+	applyStyling();
+	trackPath( store );
+	reloadOnCacheUpdate();
 }
