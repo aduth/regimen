59a6b4752b2cf40dce74b767391f612aaec388a5
Andrew Duthie <aduthie7@gmail.com>
2016-02-26 20:27:16 -0500

    Remove EditableLabel component from PlanPageHeader
    Undesirable, as editing title should not be a simple mechanism, as it
    creates an entirely new plan

--- src/components/plan-page-header/index.jsx
+++ src/components/plan-page-header/index.jsx
@@ -5,19 +5,16 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import get from 'lodash/get';
 
 /**
  * Internal dependencies
  */
 
-import { editPlan } from 'state/plans/actions';
 import { getPlanId } from 'state/ui/selectors';
 import { getPlan, isPlanNotFound } from 'state/plans/selectors';
-import EditableLabel from 'components/editable-label';
 
-function PlanPageHeader( { planId, title, editPlan, loading, notFound } ) {
+function PlanPageHeader( { title, loading, notFound } ) {
 	const classes = classNames( 'plan-page-header', {
 		'is-loading': loading
 	} );
@@ -30,38 +27,23 @@ function PlanPageHeader( { planId, title, editPlan, loading, notFound } ) {
 
 	return (
 		<span className={ classes }>
-			<EditableLabel
-				value={ title }
-				onChange={ ( value ) => {
-					editPlan( planId, { title: value } )
-				} } />
+			{ title }
 		</span>
 	);
 }
 
 PlanPageHeader.propTypes = {
-	planId: PropTypes.string,
 	title: PropTypes.string,
-	editPlan: PropTypes.func,
 	loading: PropTypes.bool,
 	notFound: PropTypes.bool
-};
-
-PlanPageHeader.defaultProps = {
-	editPlan: () => {}
 };
 
 export default connect( ( state ) => {
 	const planId = getPlanId( state );
 	const plan = getPlan( state, planId );
 	return {
-		planId,
 		title: get( plan, 'title', '' ),
 		loading: ! plan,
 		notFound: isPlanNotFound( state, planId )
 	};
-}, ( dispatch ) => {
-	return bindActionCreators( {
-		editPlan
-	}, dispatch );
 } )( PlanPageHeader );
