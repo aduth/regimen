aff129dfd6d40ee382dca6536890baa15afde13d
Andrew Duthie <aduthie7@gmail.com>
2016-03-25 04:36:47 -0400

    Trim leading whitespace from output markup

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -40,32 +40,32 @@ module.exports = {
 					'<html manifest="/appcache/manifest.appcache">' :
 					'<html>' ) +
 				'<head>' +
-				'	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">' +
-				'	<meta name="description" content="Regimen helps with your workout progression planning. Supporting many popular routines, achieve your goals with a simple-to-use interface.">' +
-				'	<meta name="theme-color" content="#63bdd4">' +
-				'	<meta name="apple-mobile-web-app-capable" content="yes">' +
-				'	<meta name="apple-mobile-web-app-title" content="Regimen">' +
-				'	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">' +
-				'	<title>Regimen</title>' +
-				'	<link rel="dns-prefetch" href="https://regimenapp.cloudant.com">' +
-				'	<link rel="manifest" href="/manifest.json">' +
-				'	<link rel="icon" href="/favicon.ico" type="image/x-icon">' +
-				'	<link rel="apple-touch-icon" href="/images/icon-48.png">' +
-				'	<link rel="apple-touch-icon" sizes="96x96" href="/images/icon-96.png">' +
-				'	<link rel="apple-touch-icon" sizes="192x192" href="/images/icon-192.png">' +
-				'	<link rel="apple-touch-startup-image" href="/images/launch-1242.png"' +
-				'		media="(device-width: 414px) and (device-height: 736px)' +
-				'			and (-webkit-device-pixel-ratio: 3)' +
-				'			and (orientation: landscape)">' +
-				'	<link rel="apple-touch-startup-image" href="/images/launch-750.png"' +
-				'		media="(device-width: 375px) and (device-height: 667px)' +
-				'			and (-webkit-device-pixel-ratio: 2)">' +
-				'	<link rel="apple-touch-startup-image" href="/images/launch-640.png"' +
-				'		media="(device-width: 320px) and (device-height: 568px)' +
-				'			and (-webkit-device-pixel-ratio: 2)">' +
-				'	{% for ( var css in o.htmlWebpackPlugin.files.css ) { %}' +
-				'	<link rel="stylesheet" href="{%= o.htmlWebpackPlugin.files.css[ css ] %}">' +
-				'	{% } %}' +
+					'<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">' +
+					'<meta name="description" content="Regimen helps with your workout progression planning. Supporting many popular routines, achieve your goals with a simple-to-use interface.">' +
+					'<meta name="theme-color" content="#63bdd4">' +
+					'<meta name="apple-mobile-web-app-capable" content="yes">' +
+					'<meta name="apple-mobile-web-app-title" content="Regimen">' +
+					'<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">' +
+					'<title>Regimen</title>' +
+					'<link rel="dns-prefetch" href="https://regimenapp.cloudant.com">' +
+					'<link rel="manifest" href="/manifest.json">' +
+					'<link rel="icon" href="/favicon.ico" type="image/x-icon">' +
+					'<link rel="apple-touch-icon" href="/images/icon-48.png">' +
+					'<link rel="apple-touch-icon" sizes="96x96" href="/images/icon-96.png">' +
+					'<link rel="apple-touch-icon" sizes="192x192" href="/images/icon-192.png">' +
+					'<link rel="apple-touch-startup-image" href="/images/launch-1242.png"' +
+						'media="(device-width: 414px) and (device-height: 736px)' +
+							'and (-webkit-device-pixel-ratio: 3)' +
+							'and (orientation: landscape)">' +
+					'<link rel="apple-touch-startup-image" href="/images/launch-750.png"' +
+						'media="(device-width: 375px) and (device-height: 667px)' +
+							'and (-webkit-device-pixel-ratio: 2)">' +
+					'<link rel="apple-touch-startup-image" href="/images/launch-640.png"' +
+						'media="(device-width: 320px) and (device-height: 568px)' +
+							'and (-webkit-device-pixel-ratio: 2)">' +
+					'{% for ( var css in o.htmlWebpackPlugin.files.css ) { %}' +
+					'<link rel="stylesheet" href="{%= o.htmlWebpackPlugin.files.css[ css ] %}">' +
+					'{% } %}' +
 				'</head>' +
 				'<body>' +
 				'<div id="app"></div>' +
